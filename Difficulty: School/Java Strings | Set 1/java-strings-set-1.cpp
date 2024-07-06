3c086c7d5c40a70ddab14250ff412932bddfdb53
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-07-06 18:27:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Java Strings | Set 1/java-strings-set-1.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string conRevstr(string S1, string S2) {
+        // code here
+        string S3 = S1+=S2;
+        
+        for(int i=0;i<S3.length()/2;i++)
+        {
+            swap(S3[i],S3[S3.length()-i-1]);
+        }
+        return S3;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S1, S2;
+        cin >> S1;
+        cin >> S2;
+        Solution ob;
+        cout << ob.conRevstr(S1, S2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
