ee138e51cccba3e45fc6fcdb1da4d643b0f82315
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-07-20 15:58:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximize Toys/maximize-toys.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int toyCount(int N, int K, vector<int> arr)
+    {
+        // code here
+         sort(arr.begin(), arr.end());
+        int i = 0, sum = 0;
+        while(sum < K && i < N) sum += arr[i++];
+        return i < N ? i - 1 : i;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, K;
+        cin>>N>>K;
+        vector<int> arr(N);
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.toyCount(N, K, arr)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
