206d074ffe16ab0ca5b5d6de9272c5a7f866e257
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-08-04 23:56:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -2,40 +2,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to find the maximum number of meetings that can
-    //be performed in a meeting room.
-    static bool cmp(pair<int,int> a, pair<int,int> b)
-    {
-        return a.second < b.second;
-    }
-    int maxMeetings(int start[], int end[], int n)
-    {
+class Solution {
+  public:
+    // Function to find the maximum number of meetings that can
+    // be performed in a meeting room.
+    int maxMeetings(int n, int start[], int end[]) {
         // Your code here
-        vector<pair<int,int> > v;
-        for(int i=0;i<n;i++)
-        {
-            pair<int,int> p = make_pair(start[i],end[i]);
-            v.push_back(p);
+        vector<pair<int,int>> v;
+        int ans = 1;
+        
+        for(int i = 0; i < n;i++){
+            v.push_back({end[i],start[i]});
+        }
+        sort(v.begin(),v.end());
+        
+        int lastTime = v[0].first;
+        
+        for(int i = 1; i < n; i++){
+             if(lastTime < v[i].second){
+                lastTime = v[i].first;
+                ans++;
+             }
+            
         }
         
-        sort(v.begin(), v.end(),cmp);
-        
-        int count = 1;
-        int ansend = v[0].second;
-        
-        for(int i=1;i<n;i++)
-        {
-            if(v[i].first>ansend)
-            {
-                count++;
-                ansend = v[i].second;
-            }
-        }
-        return count;
+        return ans;
     }
 };
 
@@ -47,12 +40,14 @@ int main() {
         int n;
         cin >> n;
         int start[n], end[n];
-        for (int i = 0; i < n; i++) cin >> start[i];
+        for (int i = 0; i < n; i++)
+            cin >> start[i];
 
-        for (int i = 0; i < n; i++) cin >> end[i];
+        for (int i = 0; i < n; i++)
+            cin >> end[i];
 
         Solution ob;
-        int ans = ob.maxMeetings(start, end, n);
+        int ans = ob.maxMeetings(n, start, end);
         cout << ans << endl;
     }
     return 0;
