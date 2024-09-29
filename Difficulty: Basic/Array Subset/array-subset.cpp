eb5f8ffda927b5033b484e28756c1f5f7bd8ef72
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-09-29 23:35:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Subset/array-subset.cpp
+++ Difficulty: Basic/Array Subset/array-subset.cpp
@@ -29,40 +29,29 @@ int main() {
 
 
 string isSubset(int a1[], int a2[], int n, int m) {
-    sort(a1, a1 + n); 
-    sort(a2, a2 + m); 
-  
-    
-    int i = 0, j = 0; 
-  
-   
-    while (i < n && j < m) 
-    { 
-    
-        if (a1[i] == a2[j]) 
-        { 
-            i++; 
-            j++; 
-        } 
-  
-        else if (a1[i] < a2[j]) 
-        { 
-            i++; 
-        } 
-  
-      
-        else
-        { 
-            return "No"; 
-        } 
-    } 
-  
-
-    if (j == m) 
-        return "Yes"; 
-  
-    else
-        return "No";
-    
-    
+    int i=0,j=0,count=0;
+    sort(a1,a1+n);
+    sort(a2,a2+m);
+    while(j<m)
+    {
+        if(a1[i]==a2[j])
+        {
+            i++;
+            j++;
+            count++;
+        }
+        else if(a1[i]<a2[j])
+        {
+            i++;
+        }
+        else if(a1[i]>a2[j])
+        {
+            j++;
+        }
+    }
+    if(count==m)
+    {
+        return "Yes";
+    }
+    return "No";
 }
