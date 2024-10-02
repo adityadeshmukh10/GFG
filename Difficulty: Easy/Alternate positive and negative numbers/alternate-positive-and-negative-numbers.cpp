d484fb6a2f4c04eb00e220a40f6d1a68fc7f2d4b
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-10-02 11:51:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Alternate positive and negative numbers/alternate-positive-and-negative-numbers.cpp
+++ Difficulty: Easy/Alternate positive and negative numbers/alternate-positive-and-negative-numbers.cpp
@@ -3,34 +3,44 @@
 
 using namespace std;
 
-// } Driver Code Ends
-//User function template for C++
-class Solution{
-public:
 
-	void rearrange(int arr[], int n) {
-	    // code here
-	    vector<int> pos;
-	   vector<int> neg;
-	   for(int i=0; i<n; i++){
-	       if(arr[i]<0) neg.push_back(arr[i]);
-	       else pos.push_back(arr[i]);
-	   }
-	   int i=0;
-	   int j=0, k=0;
-	   while(i<n){
-	       if(j<pos.size()){
-	           arr[i] = pos[j];
-	           i++;
-	           j++;
-	       } 
-	       if(k<neg.size()){
-	           arr[i] = neg[k];
-	           i++;
-	           k++;
-	       }
-	   }
-	}
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    void rearrange(vector<int> &arr) {
+        // code here
+        int j=0,k=0;
+        int n = arr.size();
+	    vector<int> positive;
+	    vector<int> negative;
+	    
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]>=0)
+	        {
+	            positive.push_back(arr[i]);
+	        }
+	        else
+	        {
+	            negative.push_back(arr[i]);
+	        }
+	    }
+	    
+	    int i=0;
+	    while(i<n)
+	    {
+	        if(j<positive.size())
+	        {
+	            arr[i++] = positive[j++];
+	        }
+	        if(k<negative.size())
+	        {
+	            arr[i++] = negative[k++];
+	        }
+	    }
+    }
+    
 };
 
 //{ Driver Code Starts.
@@ -38,16 +48,20 @@ public:
 int main() {
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        int n, i;
-        cin >> n;
-        int arr[n];
-        for (i = 0; i < n; i++) {
-            cin >> arr[i];
+        vector<int> arr;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
         }
         Solution ob;
-        ob.rearrange(arr, n);
-        for (i = 0; i < n; i++) {
+        ob.rearrange(arr);
+        for (int i = 0; i < arr.size(); i++) {
             cout << arr[i] << " ";
         }
         cout << "\n";
