f7108fcef1e6d0ed1e668eff97c60e776bd78fd5
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2025-04-25 23:12:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Identical Linked Lists/identical-linked-lists.cpp
+++ Difficulty: Basic/Identical Linked Lists/identical-linked-lists.cpp
@@ -1,13 +1,16 @@
 //{ Driver Code Starts
-#include <bits/stdc++.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
+
+#include <iostream>
+#include <sstream>
+#include <stdio.h> // For freopen function
+#include <string>
+#include <vector>
+
 using namespace std;
 
 struct Node {
     int data;
-    struct Node *next;
+    struct Node* next;
 
     Node(int x) {
         data = x;
@@ -15,74 +18,93 @@ struct Node {
     }
 };
 
-bool areIdentical(struct Node *head1, struct Node *head2);
+// Function to build a linked list from input
+Node* buildList() {
+    vector<int> arr;
+    string input;
+    getline(cin, input);    // Read the entire line as input
+    stringstream ss(input); // Use stringstream to split the input into integers
+    int number;
+    while (ss >> number) {
+        arr.push_back(number); // Add each number to the vector
+    }
+    if (arr.empty()) {
+        return NULL; // Return NULL if the vector is empty
+    }
+
+    // Create the head node
+    Node* head = new Node(arr[0]);
+    Node* tail = head;
+
+    // Create remaining nodes
+    for (size_t i = 1; i < arr.size(); i++) {
+        tail->next = new Node(arr[i]);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+
+// } Driver Code Ends
+
+/* node for linked list:
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution {
+  public:
+    // Function to check if two linked lists are identical
+    bool areIdentical(struct Node *head1, struct Node *head2) {
+        
+        Node* temp1=head1;
+        Node* temp2=head2;
+        
+        while(temp1!=NULL && temp2!=NULL)
+        {
+            if(temp1->data==temp2->data)
+            {
+                temp1=temp1->next;
+                temp2=temp2->next;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+};
+
+
+//{ Driver Code Starts.
 
 int main() {
+
     int T;
     cin >> T;
-    while (T--) {
-        int n1, n2, tmp, d1, d2;
-        struct Node *head1 = NULL, *tail1 = NULL;
-        struct Node *head2 = NULL, *tail2 = NULL;
-        cin >> n1;
-        cin >> d1;
-        head1 = new Node(d1);
-        tail1 = head1;
-        while (n1-- > 1) {
-            cin >> tmp;
-            tail1->next = new Node(tmp);
-            tail1 = tail1->next;
-        }
-        cin >> n2;
-        cin >> d2;
-        head2 = new Node(d2);
-        tail2 = head2;
-        while (n2-- > 1) {
-            cin >> tmp;
-            tail2->next = new Node(tmp);
-            tail2 = tail2->next;
-        }
+    cin.ignore(); // Ignore newline after reading T to use getline properly
 
-        areIdentical(head1, head2) ? cout << "true" << endl : cout << "false" << endl;
+    while (T--) {
+        Node* head1 = buildList(); // Build first list from input
+        Node* head2 = buildList(); // Build second list from input
+        Solution ob;               // Directly create the object (no need for `new`)
+        ob.areIdentical(head1, head2) ? cout << "true" << endl
+                                      : cout << "false" << endl;
+
+        cout << "~" << endl;
     }
+
     return 0;
 }
 
 // } Driver Code Ends
-
-
-/*
-Structure of the node of the linked list is as
-struct Node {
-  int data;
-  struct Node *next;
-  Node(int x) {
-    data = x;
-    next = NULL;
-  }
-};
-*/
-
-// Function to check whether two linked lists are identical or not.
-bool areIdentical(struct Node *head1, struct Node *head2) {
-    // Code here
-    while(head1!=NULL && head2!=NULL)
-
-    {
-
-        if(head1->data!=head2->data)
-
-            return false;
-
-        head1=head1->next;
-
-        head2=head2->next;
-
-    }
-
-    if(head1==NULL && head2==NULL)
-
-        return true;
-
-    return false;
-}
